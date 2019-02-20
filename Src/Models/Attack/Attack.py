2910f9404c2d87c9cdde0213ad834572fe041151
laedvin <edvin.lam.el@gmail.com>
2019-02-20 19:04:07 +0100

    Kan nu attackera en specifik koordinat. Kräver inte längre dess egna baskoordinat. En tester är också inkluderad som körs med "python -m Models.Attack.TestAttack".

--- Src/Models/Attack/Attack.py
+++ Src/Models/Attack/Attack.py
@@ -9,15 +9,36 @@ class Attack(base_agent.BaseAgent):
         self.reqSteps = 0
         self.minimap_size = 64
 
-    def attack(self, obs, base_minimap):
+    def attack(self, obs, location=None):
         """Selects all army units and issues an attack order on the closest enemy.
-        It checks for enemies using the minimap.
+                It checks for enemies using the minimap.
 
-        :param obs: The observer.
-        :param base_minimap: The bot's base location [x, y] in minimap coordinates. Used to find closest enemy.
-        :return: Returns an action (either select_army or Attack_minimap).
-        """
+                :param obs: The observer.
+                :param location: The desired location to attack [x, y] in minimap coordinates.
+                                   If None, it attacks the closest enemy
+                :return: Returns an action (either select_army or Attack_minimap).
+                """
         new_action = actions.FUNCTIONS.no_op()
+
+        if location is None:
+            enemy_x = []
+            enemy_y = []
+            distance = []
+
+            for x in range(self.minimap_size):
+                for y in range(self.minimap_size):
+                    if obs.observation.feature_minimap[5][x][y] == 4:  # Finds enemy units on minimap
+                        enemy_x.append(x)
+                        enemy_y.append(y)
+                        distance.append(np.power(x - self.base_location[0], 2) + np.power(y - self.base_location[1], 2))
+
+            if len(distance) > 0:
+                index_min = min(range(len(distance)), key=distance.__getitem__)
+                location = [enemy_x[index_min], enemy_y[index_min]]
+
+            else:
+                location = self.base_location
+
         if self.reqSteps == 0:
             self.reqSteps = 2
 
@@ -29,21 +50,6 @@ class Attack(base_agent.BaseAgent):
         elif self.reqSteps == 1:
             self.reqSteps = 0
             if actions.FUNCTIONS.Attack_minimap.id in obs.observation.available_actions:
-                enemy_x = []
-                enemy_y = []
-                distance = []
-                for x in range(self.minimap_size):
-                    for y in range(self.minimap_size):
-                        if obs.observation.feature_minimap[5][x][y] == 4:  # Finds enemy units on minimap
-                            enemy_x.append(x)
-                            enemy_y.append(y)
-                            distance.append(np.power(x-base_minimap[0], 2) + np.power(y-base_minimap[1], 2))
-                if len(distance) > 0:
-                    index_min = min(range(len(distance)), key=distance.__getitem__)
-                    enemy_location = [enemy_y[index_min], enemy_x[index_min]]
-
-                else:
-                    enemy_location = [32, 32]
-                new_action = actions.FUNCTIONS.Attack_minimap("now", enemy_location)
+                new_action = actions.FUNCTIONS.Attack_minimap("now", [location[1], location[0]])
 
         return new_action
---
+++ Src/Models/Attack/TestAttack.py
@@ -0,0 +1,73 @@
+from pysc2.env import sc2_env
+from pysc2.agents import base_agent
+from pysc2.lib import features, actions
+from Models.Attack.Attack import Attack
+from absl import app
+import numpy as np
+
+class TestAttack(base_agent.BaseAgent):
+    def __init__(self):
+        super(TestAttack, self).__init__()
+        self.base_location = None
+        self.reqSteps = 0
+        self.minimap_size = 64
+        self.has_attacked = False
+        self.attacking = False
+
+    def step(self, obs):
+        super(TestAttack, self).step(obs)
+        if obs.first():
+            start_y, start_x = (obs.observation.feature_minimap.player_relative
+                                == features.PlayerRelative.SELF).nonzero()
+            xmean = start_x.mean()
+            ymean = start_y.mean()
+
+            self.base_location = (xmean, ymean)
+
+        if self.reqSteps == 0 and self.has_attacked:
+            self.attacking = False
+
+        if self.reqSteps == 0 and not self.has_attacked:
+            self.attacking = True
+
+        if self.attacking:
+            return Attack.attack(self, obs, [3, 3])
+
+        return actions.FUNCTIONS.no_op()
+
+
+def main(unused_argv):
+    max_episodes = 10
+    episode = 0
+    try:
+        with sc2_env.SC2Env(
+                map_name="UnitSpawnerMarine",
+                players=[sc2_env.Agent(sc2_env.Race.terran),
+                         sc2_env.Bot(sc2_env.Race.random,
+                                     sc2_env.Difficulty.very_easy)],
+                agent_interface_format=features.AgentInterfaceFormat(
+                    feature_dimensions=features.Dimensions(screen=84, minimap=64),
+                    use_feature_units=True, ),
+                step_mul=5,
+                game_steps_per_episode=0,
+                visualize=True) as env:
+            while episode < max_episodes:
+                episode += 1
+                print("Episode "+str(episode)+"/"+str(max_episodes))
+                agent = TestAttack()
+                agent.setup(env.observation_spec(), env.action_spec())
+
+                timesteps = env.reset()
+                agent.reset()
+
+                while True:
+                    step_actions = [agent.step(timesteps[0])]
+                    if timesteps[0].last():
+                        break
+                    timesteps = env.step(step_actions)
+
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    app.run(main)
