bb24899b9f7829123f056916c2dad1940af870a0
CarlClaesson <claesson.carl@hotmail.com>
2019-02-22 11:19:41 +0100

    Saving replays work if you define path

--- Src/mainLoop.py
+++ Src/mainLoop.py
@@ -17,6 +17,9 @@ def main(unused_argv):
                         use_feature_units=True),
                     step_mul=5,  #about 200 APM
                     game_steps_per_episode=80000, # about 1 h before game ends
+                    #save_replay_episodes=1, #How often do you save replays
+                    #replay_dir="C:/Users/Claes/Desktop/StarCraft2Replays", # Need to change to your own path, notice forward slashes instead of backslashes!
+                    #You can visualize replay with command "python -m pysc2.bin.play --replay "C:/Users/Claes/Desktop/StarCraft2Replays/AbyssalReef_2019-02-22-09-42-52.SC2Replay"" (but with your own path)
                     visualize=True) as env:
 
                 agent.setup(env.observation_spec(), env.action_spec())
