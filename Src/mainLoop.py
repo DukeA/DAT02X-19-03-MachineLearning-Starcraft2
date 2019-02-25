e1fc3d0b60c599fd54f79b2f0e7bc37d02540a0a
laedvin <edvin.lam.el@gmail.com>
2019-02-25 21:22:13 +0100

    ArmyControl är nog singleton nu. Förbättrat attack-metoden. Implementerat den i boten.

--- Src/mainLoop.py
+++ Src/mainLoop.py
@@ -6,19 +6,19 @@ from Models.BotFile.aiBot import AiBot
 def main(unused_argv):
     agent = AiBot()
     try:
-        while True:
-            with sc2_env.SC2Env(
-                    map_name="AbyssalReef",
-                    players=[sc2_env.Agent(sc2_env.Race.terran),
-                             sc2_env.Bot(sc2_env.Race.terran,
+        with sc2_env.SC2Env(
+                map_name="AbyssalReef",
+                players=[sc2_env.Agent(sc2_env.Race.terran),
+                         sc2_env.Bot(sc2_env.Race.terran,
                                          sc2_env.Difficulty.very_easy)],
-                    agent_interface_format=features.AgentInterfaceFormat(
-                        feature_dimensions=features.Dimensions(screen=84, minimap=64),
-                        use_feature_units=True),
-                    step_mul=1,
-                    game_steps_per_episode=0,
-                    visualize=True) as env:
-
+                agent_interface_format=features.AgentInterfaceFormat(
+                    feature_dimensions=features.Dimensions(screen=84, minimap=64),
+                    use_feature_units=True),
+                step_mul=5,
+                game_steps_per_episode=0,
+                visualize=True,
+                disable_fog=True) as env:
+            while True:
                 agent.setup(env.observation_spec(), env.action_spec())
 
                 timesteps = env.reset()
