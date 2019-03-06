43b3b13cf412b0799eedd64387f761eb19b7a7f5
laedvin <edvin.lam.el@gmail.com>
2019-03-06 21:20:42 +0100

    Added more IsPossible checks in the selector.

--- Src/Models/Selector/buildSelector.py
+++ Src/Models/Selector/buildSelector.py
@@ -13,40 +13,59 @@ class BuildSelector():
         poss_actions = ["do_nothing"]
         if IsPossible.build_scv_possible(self, obs):
             poss_actions.append("build_scv")
+
         if IsPossible.build_supply_depot_possible(self, obs):
             poss_actions.append("build_supply_depot")
+
         if IsPossible.build_marines_possible(self, obs):
             poss_actions.append("build_marine")
+
         if IsPossible.build_marauder_possible(self, obs):
             poss_actions.append("build_marauder")
-        if True:
+
+        if IsPossible.build_reaper_possible(self, obs):
             poss_actions.append("build_reaper")
-        if True:
+
+        if IsPossible.build_hellion_possible(self, obs):
             poss_actions.append("build_hellion")
+
         if IsPossible.build_medivac_possible(self, obs):
             poss_actions.append("build_medivac")
-        if True:
+
+        if IsPossible.build_viking_possible(self, obs):
             poss_actions.append("build_viking")
+
         if True:
             poss_actions.append("transform_viking_to_ground")
+
         if True:
             poss_actions.append("transform_viking_to_air")
+
         if IsPossible.build_barracks_possible(self, obs):
             poss_actions.append("build_barracks")
+
         if IsPossible.build_refinery_possible(self, obs):
             poss_actions.append("build_refinery")
+
         if True:
             poss_actions.append("distribute_scv")
+
         if True:
             poss_actions.append("return_scv")
+
         if IsPossible.build_command_center_possible(self, obs):
             poss_actions.append("expand")
+
         if IsPossible.build_factory_possible(self, obs):
             poss_actions.append("build_factory")
+
         if IsPossible.build_starport_possible(self, obs):
             poss_actions.append("build_starport")
+
         if IsPossible.build_techlab_possible(self, obs):
             poss_actions.append("build_tech_lab")
+
         if True:
             poss_actions.append("no_op")
+
         return poss_actions
