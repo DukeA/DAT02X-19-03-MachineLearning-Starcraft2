7b2ee8cc2466b8c67d8c4475eaccb4d331a53931
Adam Grandén <adam.granden@hotmail.com>
2019-03-10 16:13:53 +0100

    Started on the build Order
    Started on a build order

---
+++ Src/BuildNetwork/BuildingQueue.py
@@ -0,0 +1,17 @@
+
+from enum import  Enum
+from pysc2.lib import units
+
+
+class BuildingQueue(Enum):
+    commandcenter = units.Terra.CommandCenter,
+    barracks = units.Terran.Barracks,
+    barracks_techlab = units.Terran.BarracksTechLab,
+    factory = units.Terran.Factory,
+    refinary = units.Terran.Refinery,
+    starPort = units.Terran.Starport,
+    supply_depot = units.Terran.supplydepot
+
+
+
+
