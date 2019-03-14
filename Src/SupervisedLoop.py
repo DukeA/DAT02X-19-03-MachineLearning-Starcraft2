ae964cbdbb823b59a3223afa54dc52281c7d4ad7
laedvin <edvin.lam.el@gmail.com>
2019-03-14 17:01:24 +0100

    Small changes for new training.

--- Src/SupervisedLoop.py
+++ Src/SupervisedLoop.py
@@ -8,7 +8,7 @@ import pickle
 def main(unused_argv):
     agent = AiBot()
     episode = 0
-    file_name_offset = 202
+    file_name_offset = 0
     save_data = True
     try:
         with sc2_env.SC2Env(
@@ -34,12 +34,12 @@ def main(unused_argv):
 
                 while True:
                     step_actions = [agent.step(timesteps[0])]
-                    if agent.game_state.units_amount[units.Terran.Marine.value] >= 5 and\
-                            agent.game_state.units_amount[units.Terran.SCV.value] >= 20:
-                        print("5 marines at agent step "+str(agent.steps))
+                    if agent.game_state.units_amount[units.Terran.Marine.value] >= 10 and\
+                            agent.game_state.units_amount[units.Terran.SCV.value] >= 16:
+                        print(">=10 marines, >=16 SCVs at agent step "+str(agent.steps))
                         if save_data:
                             with open("C:/Users/Edvin/Documents/Python/SC2MachineLearning/" +
-                                      "DAT02X-19-03-MachineLearning-Starcraft2/Src/TrainingData/marinetestII" +
+                                      "DAT02X-19-03-MachineLearning-Starcraft2/Src/TrainingData/10Marines/random_game" +
                                       str(episode+file_name_offset)+".data", 'wb') as filehandle:
                                 pickle.dump(agent.game_state.get_state(), filehandle)
                         break
