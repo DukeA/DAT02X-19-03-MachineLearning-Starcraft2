Actor-Critic policy gradient agent fixes

Src/Models/MachineLearning/ActorCriticAgent.py
@@ -18,7 +18,7 @@ import timeit
 OU = OU()       #Ornstein-Uhlenbeck Process
 
 class ActorCriticAgent:
-    def __init__(self):
+    def __init__(self, action_dim, state_dim, action_space):
         self.train_indicator = True
         self.BUFFER_SIZE = 100000
         self.BATCH_SIZE = 32
@@ -27,10 +27,10 @@ class ActorCriticAgent:
         self.LRA = 0.0001    #Learning rate for Actor
         self.LRC = 0.001     #Lerning rate for Critic
 
-        self.action_dim = 3  #Steering/Acceleration/Brake
-        self.state_dim = 29  #of sensors input
+        self.action_dim = action_dim
+        self.state_dim = state_dim  # TODO Pass variable from State to set this automatically
 
-        self.np.random.seed(1337)
+        np.random.seed(1337)
 
         self.vision = False
 
@@ -42,6 +42,8 @@ class ActorCriticAgent:
         self.step = 0
         self.epsilon = 1
         self.indicator = 0
+        self.episode = 0
+        self.action_space = action_space
 
         # Tensorflow GPU optimization
         self.config = tf.ConfigProto()
@@ -56,7 +58,7 @@ class ActorCriticAgent:
 
         self.total_reward = 0
         self.prev_state = None
-        self.prev_action = None
+        self.prev_actions = None
 
 
         # Now load the weight
@@ -76,99 +78,68 @@ class ActorCriticAgent:
         print("TORCS Experiment Start.")
 
     def predict(self, game_state):
-        #for i in range(episode_count):
 
-            #print("Episode : " + str(i) + " Replay Buffer " + str(buff.count()))
+        state = np.hstack((game_state.minerals, game_state.vespene))
 
-            #if np.mod(i, 3) == 0:
-            #    ob = env.reset(relaunch=True)  # relaunch TORCS every 3 episode because of the memory leak error
-            #else:
-            #    ob = env.reset()
+        loss = 0
 
-            #s_t = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY,  ob.speedZ, ob.wheelSpinVel/100.0, ob.rpm))
+        action_probabilities = self.actor.model.predict(state.reshape(1, state.shape[0]))
+
+        if self.episode == 0:
+            self.prev_actions = action_probabilities
+            self.prev_state = state
+
+        else:
+            self.buff.add(self.prev_state, self.prev_actions[0], game_state.reward, state, False)  # Add replay buffer
+
+            self.prev_actions = action_probabilities
+            self.prev_state = state
+
+            # Do the batch update
+            batch = self.buff.getBatch(self.BATCH_SIZE)
+            states = np.asarray([e[0] for e in batch])
+            actions = np.asarray([e[1] for e in batch])
+            rewards = np.asarray([e[2] for e in batch])
+            new_states = np.asarray([e[3] for e in batch])
+            dones = np.asarray([e[4] for e in batch])
+            y_t = np.asarray([e[1] for e in batch])
+
+            target_q_values = self.critic.target_model.predict([new_states, self.actor.target_model.predict(new_states)])
+
+            for k in range(len(batch)):
+                if dones[k]:
+                    y_t[k] = rewards[k]
+                else:
+                    y_t[k] = rewards[k] + self.GAMMA*target_q_values[k]
+
+            if self.train_indicator:
+                loss += self.critic.model.train_on_batch([states, actions], y_t)
+                a_for_grad = self.actor.model.predict(states)
+                grads = self.critic.gradients(states, a_for_grad)
+                self.actor.train(states, grads)
+                self.actor.target_train()
+                self.critic.target_train()
+
+            self.total_reward += game_state.reward
+
+        self.episode += 1
+
+        max_index = np.argmax(action_probabilities)
+
+        chosen_action = self.action_space[max_index]
+
+        return chosen_action
+
+        #if np.mod(self.episode, 3) == 0:
+        #    if (self.train_indicator):
+        #        print("Now we save the model")
+         #       self.actor.model.save_weights("actormodel.h5", overwrite=True)
+        ##            json.dump(self.actor.model.to_json(), outfile)
+#
+        #        self.critic.model.save_weights("criticmodel.h5", overwrite=True)
+         #       with open("criticmodel.json", "w") as outfile:
+         #           json.dump(self.critic.model.to_json(), outfile)
 
 
-            #for j in range(max_steps):
-                state = [game_state.minerals]
-
-                loss = 0
-                self.epsilon -= 1.0 / self.EXPLORE
-                #a_t = np.zeros([1,self.action_dim])
-                noise_t = np.zeros([1,self.action_dim])
-
-                chosen_action = self.actor.model.predict(state.reshape(1, state.shape[0]))
-
-                #noise_t[0][0] = self.train_indicator * max(self.epsilon, 0) * OU.function(a_t_original[0][0],  0.0 , 0.60, 0.30)
-                #noise_t[0][1] = self.train_indicator * max(self.epsilon, 0) * OU.function(a_t_original[0][1],  0.5 , 1.00, 0.10)
-                #noise_t[0][2] = self.train_indicator * max(self.epsilon, 0) * OU.function(a_t_original[0][2], -0.1 , 1.00, 0.05)
-
-                # The following code do the stochastic brake
-                # if random.random() <= 0.1:
-                #    print("********Now we apply the brake***********")
-                #    noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2],  0.2 , 1.00, 0.10)
-
-                #a_t[0][0] = a_t_original[0][0] + noise_t[0][0]
-                #a_t[0][1] = a_t_original[0][1] + noise_t[0][1]
-                #a_t[0][2] = a_t_original[0][2] + noise_t[0][2]
-
-                #ob, r_t, done, info = env.step(a_t[0])
-
-                #s_t1 = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ, ob.wheelSpinVel/100.0, ob.rpm))
-
-                self.buff.add(state, chosen_action, reward, s_t1, done)      #Add replay buffer
-
-                #Do the batch update
-                batch = self.buff.getBatch(self.BATCH_SIZE)
-                states = np.asarray([e[0] for e in batch])
-                actions = np.asarray([e[1] for e in batch])
-                rewards = np.asarray([e[2] for e in batch])
-                new_states = np.asarray([e[3] for e in batch])
-                dones = np.asarray([e[4] for e in batch])
-                y_t = np.asarray([e[1] for e in batch])
-
-                target_q_values = self.critic.target_model.predict([new_states, self.actor.target_model.predict(new_states)])
-
-                for k in range(len(batch)):
-                    if dones[k]:
-                        y_t[k] = rewards[k]
-                    else:
-                        y_t[k] = rewards[k] + self.GAMMA*target_q_values[k]
-
-                if self.train_indicator:
-                    loss += self.critic.model.train_on_batch([states,actions], y_t)
-                    a_for_grad = self.actor.model.predict(states)
-                    grads = self.critic.gradients(states, a_for_grad)
-                    self.actor.train(states, grads)
-                    self.actor.target_train()
-                    self.critic.target_train()
-
-                self.total_reward += r_t
-                s_t = s_t1
-
-                print("Episode", i, "Step", step, "Action", a_t, "Reward", r_t, "Loss", loss)
-
-                step += 1
-                if done:
-                    break
-
-            if np.mod(i, 3) == 0:
-                if (train_indicator):
-                    print("Now we save model")
-                    actor.model.save_weights("actormodel.h5", overwrite=True)
-                    with open("actormodel.json", "w") as outfile:
-                        json.dump(actor.model.to_json(), outfile)
-
-                    critic.model.save_weights("criticmodel.h5", overwrite=True)
-                    with open("criticmodel.json", "w") as outfile:
-                        json.dump(critic.model.to_json(), outfile)
-
-            print("TOTAL REWARD @ " + str(i) +"-th Episode  : Reward " + str(total_reward))
-            print("Total Step: " + str(step))
-            print("")
-
-            if self.step % self.max_steps == 0:
-                # Train network
-                self.total_reward = 0
-
-        print("Finish.")
-
+# TODO Change networks to fit SC2 AI
+# TODO Keep track of episode (probably needs to be set and incremented in main_loop)

Src/Models/MachineLearning/ActorNetwork.py
@@ -21,10 +21,10 @@ class ActorNetwork(object):
 
         K.set_session(sess)
 
-        #Now create the model
-        self.model , self.weights, self.state = self.create_actor_network(state_size, action_size)   
+        # Now create the model
+        self.model, self.weights, self.state = self.create_actor_network(state_size, action_size)
         self.target_model, self.target_weights, self.target_state = self.create_actor_network(state_size, action_size) 
-        self.action_gradient = tf.placeholder(tf.float32,[None, action_size])
+        self.action_gradient = tf.placeholder(tf.float32, [None, action_size])
         self.params_grad = tf.gradients(self.model.output, self.weights, -self.action_gradient)
         grads = zip(self.params_grad, self.weights)
         self.optimize = tf.train.AdamOptimizer(LEARNING_RATE).apply_gradients(grads)
@@ -39,19 +39,20 @@ class ActorNetwork(object):
     def target_train(self):
         actor_weights = self.model.get_weights()
         actor_target_weights = self.target_model.get_weights()
-        for i in xrange(len(actor_weights)):
+        for i in range(len(actor_weights)):
             actor_target_weights[i] = self.TAU * actor_weights[i] + (1 - self.TAU)* actor_target_weights[i]
         self.target_model.set_weights(actor_target_weights)
 
-    def create_actor_network(self, state_size,action_dim):
+    def create_actor_network(self, state_size, action_dim):
         print("Now we build the model")
         S = Input(shape=[state_size])   
         h0 = Dense(HIDDEN1_UNITS, activation='relu')(S)
         h1 = Dense(HIDDEN2_UNITS, activation='relu')(h0)
-        Steering = Dense(1,activation='tanh',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)  
-        Acceleration = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)   
-        Brake = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1) 
-        V = merge([Steering,Acceleration,Brake],mode='concat')          
+        # Steering = Dense(1,activation='tanh',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)
+        # Acceleration = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)
+        # Brake = Dense(1,activation='sigmoid',init=lambda shape, name: normal(shape, scale=1e-4, name=name))(h1)
+        # V = merge([Steering,Acceleration,Brake],mode='concat')
+        V = Dense(action_dim, activation='softmax')(h1)
         model = Model(input=S,output=V)
         return model, model.trainable_weights, S
 