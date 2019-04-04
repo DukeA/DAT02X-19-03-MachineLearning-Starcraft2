42ff938302314862cf1c2baa3e1f83ac8fdafc49
Arvid <luarvid@student.chalmers.se>
2019-04-04 10:11:29 +0200

    ändringar av vikter

--- Src/Models/Selector/HardCodedSelector.py
+++ Src/Models/Selector/HardCodedSelector.py
@@ -5,9 +5,9 @@ from Models.Selector.selector import Selector
 class HardCodedSelector():
 
     def hardCodedSelector(self, obs):
-        
+
         all_actions = [
-                "no_op", 
+                "no_op",
                 "build_scv",
                 "build_supply_depot",
                 "build_marine",
@@ -33,7 +33,7 @@ class HardCodedSelector():
             ]
 
         build_actions = [
-                "no_op", 
+                "no_op",
                 "build_scv",
                 "build_supply_depot",
                 "build_marine",
@@ -51,7 +51,7 @@ class HardCodedSelector():
                 "build_starport",
                 "build_tech_lab_barracks",
             ]
-        
+
 
 
         if self.reqSteps == 0 and not self.game_state_updated:
@@ -59,46 +59,86 @@ class HardCodedSelector():
         else:
             self.game_state_updated = False
 
-#################################################################################       
+#################################################################################
             if self.steps < 16 * 60 * 1 / 5 * 1.4:   #First min
                 selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
+                if obs.observation.player.idle_worker_count > 1:
                     return "return_scv"
-                
-                if selection <= 0.1:
+
+                if selection <= 0.3:
                     if IsPossible.build_scv_possible(self, obs):
                         return "build_scv"
 
-                elif selection <= 0.3:
+                elif selection <= 0.5:
                     if IsPossible.build_supply_depot_possible(self, obs):
                         return "build_supply_depot"
-                
-                elif selection <= 0.5:
+
+                elif selection <= 0.7:
                     if IsPossible.build_barracks_possible(self, obs):
                         return "build_barracks"
 
-                elif selection <= 0.6:
+                elif selection <= 0.8:
                     if IsPossible.build_refinery_possible(self, obs):
                         return "build_refinery"
-                
-                elif selection <= 0.75:
-                    return "build_factory"
-                
+
                 elif selection <= 0.95:
                     return "no_op"
 
                 elif selection <= 1.0:
                     return (random.choice(Selector.possible_build_actions(self, obs)))
-            
+
                 else:
                     return "no_op"
-    
+
 
 #########################################################################################
-        
+
             if self.steps < 16 * 60 * 2 / 5 * 1.4:   #min 2
                 selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
+                if obs.observation.player.idle_worker_count > 1:
+                    return "return_scv"
+
+                if selection <= 0.2:
+                    if IsPossible.build_scv_possible(self, obs):
+                        return "build_scv"
+
+                elif selection <= 0.25:
+                    if IsPossible.build_supply_depot_possible(self, obs):
+                        return "build_supply_depot"
+                elif selection <= 0.3:
+                    if IsPossible.build_starport_possible(self, obs):
+                        return "build_starport"
+
+                elif selection <= 0.4:
+                    if IsPossible.build_barracks_possible(self, obs):
+                        return "build_barracks"
+
+                elif selection <= 0.5:
+                    if IsPossible.build_refinery_possible(self, obs):
+                        return "build_refinery"
+
+                elif selection <= 0.6:
+                    if IsPossible.build_factory_possible(self, obs):
+                        return "build_factory"
+
+                elif selection <= 0.8:
+                    if IsPossible.build_marines_possible(self, obs):
+                        return "build_marine"
+
+                elif selection <= 0.95:
+                    return "no_op"
+
+                elif selection <= 1.0:
+                    return (random.choice(Selector.possible_build_actions(self, obs)))
+
+                else:
+                    return "no_op"
+
+
+############################################################################################3
+            if self.steps < 16 * 60 * 3 / 5 * 1.4:   #min 3
+                selection = random.random()
+                if obs.observation.player.idle_worker_count > 1:
                     return "return_scv"
 
                 if selection <= 0.1:
@@ -108,80 +148,37 @@ class HardCodedSelector():
                 elif selection <= 0.15:
                     if IsPossible.build_supply_depot_possible(self, obs):
                         return "build_supply_depot"
-                elif selection <= 0.3:
-                    if IsPossible.build_starport_possible(self, obs):
-                        return "build_starport"
-                
-                elif selection <= 0.4:
-                    if IsPossible.build_barracks_possible(self, obs):
-                        return "build_barracks"
 
                 elif selection <= 0.5:
-                    if IsPossible.build_refinery_possible(self, obs):
-                        return "build_refinery"
-                
-                elif selection <= 0.6:
-                    if IsPossible.build_factory_possible(self, obs):
-                        return "build_factory"
-                
-                elif selection <= 0.8:
                     if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"
-                
-                elif selection <= 0.95:
-                    return "no_op"
+                        return "build_marine"
 
-                elif selection <= 1.0:
-                    return (random.choice(Selector.possible_build_actions(self, obs)))
-            
-                else:
-                    return "no_op"
-
-
-############################################################################################3
-            if self.steps < 16 * 60 * 3 / 5 * 1.4:   #min 3
-                selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
-                    return "return_scv"
-
-                if selection <= 0.05:
-                    if IsPossible.build_supply_depot_possible(self, obs):
-                        return "build_supply_depot"
-
-                elif selection <= 0.3:
-                    if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"
-
-                elif selection <= 0.4:
+                elif selection <= 0.55:
                     if IsPossible.build_refinery_possible(self, obs):
                         return "build_refinery"
 
-                elif selection <= 0.5:
+                elif selection <= 0.65:
                     if IsPossible.build_factory_possible(self, obs):
                         return "build_factory"
-                
-                elif selection <= 0.6:
-                    if IsPossible.build_starport_possible(self, obs):
-                        return "build_starport"
-                
+
                 elif selection <= 0.7:
+                    if IsPossible.build_starport_possible(self, obs):
+                        return "build_starport"
+
+                elif selection <= 0.75:
                     if IsPossible.build_techlab_possible(self, obs):
                         return "build_tech_lab_barracks"
-                
-                elif selection <= 0.8:
-                    if IsPossible.build_reaper_possible(self, obs):
-                        return "build_reaper"
-                
+
                 elif selection <= 0.85:
                     if IsPossible.build_barracks_possible(self, obs):
                         return "build_barracks"
 
                 elif selection <= 0.95:
-                    return "no_op"            
+                    return "no_op"
 
                 elif selection <= 1.0:
                     return (random.choice(Selector.possible_build_actions(self, obs)))
-            
+
                 else:
                     return "no_op"
 
@@ -189,42 +186,52 @@ class HardCodedSelector():
 ###############################################################################################
             if self.steps < 16 * 60 * 4 / 5 * 1.4:   #min 4
                 selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
+                if obs.observation.player.idle_worker_count > 1:
                     return "return_scv"
+
+                if selection <= 0.025:
+                    if IsPossible.build_supply_depot_possible(self, obs):
+                        return "build_supply_depot"
 
                 elif selection <= 0.1:
                     if IsPossible.build_barracks_possible(self, obs):
                         return "build_barracks"
-                
-                elif selection <= 0.2:
+
+                elif selection <= 0.15:
                     if IsPossible.build_factory_possible(self, obs):
                         return "build_factory"
-                
-                elif selection <= 0.3:
+
+                elif selection <= 0.2:
                     if IsPossible.build_starport_possible(self, obs):
                         return "build_starport"
-                
-                elif selection <= 0.4:
+
+                elif selection <= 0.25:
                     if IsPossible.build_techlab_possible(self, obs):
                         return "build_tech_lab_barracks"
 
                 elif selection <= 0.5:
                     if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"
+                        return "build_marine"
+
 
                 elif selection <= 0.7:
                     return "no_op"
 
-                elif selection <= 0.8:
+
+                elif selection <= 0.75:
+                    if IsPossible.build_scv_possible(self, obs):
+                        return "build_scv"
+
+                elif selection <= 0.8 and not self.attacking:
                     return "retreat"
 
                 elif selection <= 0.9:
                     return "expand"
-                
+
 
                 elif selection <= 1.0:
                     return (random.choice(Selector.possible_build_actions(self, obs)))
-            
+
                 else:
                     return "no_op"
 
@@ -232,103 +239,115 @@ class HardCodedSelector():
 ############################################################################################
             if self.steps < 16 * 60 * 5 / 5 * 1.4:   #min 5
                 selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
+                if obs.observation.player.idle_worker_count > 1:
                     return "return_scv"
 
-                if selection <= 0.2:
-                    if IsPossible.build_marauder_possible(self, obs):
-                        return "build_marauder"
+                if selection <= 0.01:
+                    self.attacking = True
+                    return "attack"
 
-                elif selection <= 0.4:
-                    if IsPossible.build_viking_possible(self, obs):
-                        return "build_viking"
-                
-                elif selection <= 0.6:
-                    if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"
-
-                elif selection <= 0.8:
-                    return "no_op"
-                
-                elif selection <= 0.9:
-                    return "retreat"
-
-                elif selection <= 1.0:
-                    return (random.choice(Selector.possible_build_actions(self, obs)))
-            
-                else:
-                    return "no_op"            
-
-############################################################################################
-            if self.steps < 16 * 60 * 10 / 5 * 1.4:   #min 10
-                
-                selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
-                    return "return_scv"
-
-                if selection <= 0.1:
-                    if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"  
+                elif selection <= 0.025:
+                    if IsPossible.build_supply_depot_possible(self, obs):
+                        return "build_supply_depot"
 
                 elif selection <= 0.2:
                     if IsPossible.build_marauder_possible(self, obs):
                         return "build_marauder"
-                
-                elif selection <= 0.3:
-                    if IsPossible.build_reaper_possible(self, obs):
-                        return "build_reaper"
 
                 elif selection <= 0.4:
                     if IsPossible.build_medivac_possible(self, obs):
                         return "build_medivac"
 
-                elif selection <= 0.5:
-                    if IsPossible.build_viking_possible(self, obs):
-                        return "build_viking"
-                
-                elif selection <= 0.5:
-                    if IsPossible.build_hellion_possible(self, obs):
-                        return "build_hellion"
-                
                 elif selection <= 0.6:
+                    if IsPossible.build_marines_possible(self, obs):
+                        return "build_marine"
+
+                elif selection <= 0.8:
+                    return "no_op"
+
+
+                elif selection <= 0.85:
+                    if IsPossible.build_scv_possible(self, obs):
+                        return "build_scv"
+
+
+                elif selection <= 0.9 and not self.attacking:
+                    return "retreat"
+
+                elif selection <= 1.0:
+                    return (random.choice(Selector.possible_build_actions(self, obs)))
+
+                else:
+                    return "no_op"
+
+############################################################################################
+            if self.steps < 16 * 60 * 10 / 5 * 1.4:   #min 10
+
+                selection = random.random()
+                if obs.observation.player.idle_worker_count > 1:
+                    return "return_scv"
+
+                if selection <= 0.02:
+                    self.attacking = True
+                    return "attack"
+
+                elif selection <= 0.1:
+                    if IsPossible.build_marines_possible(self, obs):
+                        return "build_marine"
+
+                elif selection <= 0.3:
+                    if IsPossible.build_marauder_possible(self, obs):
+                        return "build_marauder"
+
+                elif selection <= 0.35:
+                    if IsPossible.build_medivac_possible(self, obs):
+                        return "build_medivac"
+
+                elif selection <= 0.4:
                     if IsPossible.build_techlab_possible(self, obs):
                         return "build_tech_lab_barracks"
 
-                elif selection <= 0.7:
+                elif selection <= 0.45:
                     if IsPossible.build_starport_possible(self, obs):
                         return "build_starport"
-                
-                elif selection <= 0.8:
+
+                elif selection <= 0.55:
                     if IsPossible.build_factory_possible(self, obs):
                         return "build_factory"
 
-                elif selection <= 0.9:
+
+                elif selection <= 0.60:
+                    if IsPossible.build_scv_possible(self, obs):
+                        return "build_scv"
+
+                elif selection <= 0.9 and not self.attacking:
                         return "retreat"
-                
-                elif selection <= 0.94 and not self.attacking:
+
+                elif selection <= 0.94:
                     return "no_op"
-                
-                elif selection <= 0.95 and not self.attacking:
-                    return "build_supply_depot"
+
+                elif selection <= 0.95:
+                    if IsPossible.build_supply_depot_possible(self, obs):
+                        return "build_supply_depot"
 
                 else:
                     return (random.choice(Selector.possible_build_actions(self, obs)))
 ##################################################################################################
 
             if self.steps < 16 * 60 * 30 / 5 * 1.4:   #min 30
-                
+
                 selection = random.random()
-                if self.earlier_action == "build_supply_depot" or self.earlier_action == "build_barracks":
+                if obs.observation.player.idle_worker_count > 1:
                     return "return_scv"
 
                 if selection <= 0.1:
                     if IsPossible.build_marines_possible(self, obs):
-                        return "build_marines"  
+                        return "build_marine"
 
                 elif selection <= 0.2:
                     if IsPossible.build_marauder_possible(self, obs):
                         return "build_marauder"
-                
+
                 elif selection <= 0.3:
                     if IsPossible.build_reaper_possible(self, obs):
                         return "build_reaper"
@@ -340,11 +359,11 @@ class HardCodedSelector():
                 elif selection <= 0.5:
                     if IsPossible.build_viking_possible(self, obs):
                         return "build_viking"
-                
+
                 elif selection <= 0.5:
                     if IsPossible.build_hellion_possible(self, obs):
                         return "build_hellion"
-                
+
                 elif selection <= 0.6:
                     if IsPossible.build_techlab_possible(self, obs):
                         return "build_tech_lab_barracks"
@@ -352,10 +371,14 @@ class HardCodedSelector():
                 elif selection <= 0.7:
                     if IsPossible.build_starport_possible(self, obs):
                         return "build_starport"
-                
+
                 elif selection <= 0.8:
                     if IsPossible.build_factory_possible(self, obs):
                         return "build_factory"
+
+                elif selection <= 0.85:
+                    if IsPossible.build_supply_depot_possible(self, obs):
+                        return "build_supply_depot"
 
                 elif selection <= 0.9 and not self.attacking:
                     return "retreat"
@@ -373,5 +396,3 @@ class HardCodedSelector():
 
             else:
                 return (random.choice(Selector.possible_build_actions(self, obs)))
-            
-         
