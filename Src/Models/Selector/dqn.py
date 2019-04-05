a0e0afd1b4c2153cddca38b6276e6bc31c60d912
Arvid <luarvid@student.chalmers.se>
2019-04-05 18:48:46 +0200

    3

--- Src/Models/Selector/dqn.py
+++ Src/Models/Selector/dqn.py
@@ -22,7 +22,7 @@ class DQN:
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.999995
+        self.epsilon_decay = 0.99995
         self.learning_rate = 0.002
         self.model = self._build_model()
 
