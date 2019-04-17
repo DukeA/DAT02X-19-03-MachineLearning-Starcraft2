8b970deed06d93948fe55109ba1ae9080ba48761
Adam Grandén <adam.granden@hotmail.com>
2019-04-17 21:08:10 +0200

    Update for buildlocation
    build location update

--- Src/Models/BuildNetwork/Build_location.py
+++ Src/Models/BuildNetwork/Build_location.py
@@ -32,7 +32,7 @@ class Build_location:
         build_location = []
         list = Build_location.get_surronding_area(self, Build_location.LIST)
         performed_build_location = Build_location().get_good_points(build_location)
-        list_locations = Build_location().get_build_areas(self,performed_build_location)
+        list_locations = Build_location().get_build_areas(performed_build_location)
         return performed_build_location
 
     """
@@ -41,21 +41,25 @@ class Build_location:
     """
 
     def get_build_areas(self, list):
-        if len(list) >= 0:
-            return None
-        build_areas = []
+        if len(list) <= 0:
+            return [(0, 0, 0)]
         n_list = list
-        for position in list:
-            return True
+        builid_location = []
+        for i in list:
+            start_location = i[2] - i[0]
+            end_location = i[2] - i[1]
+            builid_location.append(start_location, end_location, i[2])
+        return builid_location
 
     """
         :param lists -The building list with all the coordinates for the map
         :param building_location- The starting location for the pixel point of the screen
-        The algorithm  takes the points and then check if the points are there
+        The algorithm  takes the points and then add's for the locations which has the highest area free  
+        from that it then saves the algorithm into a tuple where it should be located
     """
 
     def get_surronding_area(self, lists):
-        build_areas =[]
+        build_areas = []
         done = False
         Row = len(lists)
         Column = len(lists[0])
@@ -66,7 +70,7 @@ class Build_location:
             for j in range(1, Column):
                 if lists[i][j] == 0:
                     State[i][j] = min(State[i][j - 1], State[i - 1][j],
-                                  State[i - 1][j - 1]) + 1
+                                      State[i - 1][j - 1]) + 1
                 else:
                     State[i][j] = 0
 
@@ -80,15 +84,15 @@ class Build_location:
                         max_of_s = State[i][j]
                         max_i = i
                         max_j = j
-            build_areas.append([max_i, max_j,max_of_s])
+
+            build_areas.append([max_i, max_j, max_of_s])
 
             for i in range(max_i, max_i - max_of_s, -1):
                 for j in range(max_j, max_j - max_of_s, -1):
                     State[i][j] = -1
+            for i
+            if State[81][81] == -1:
+                done = True
 
-            for i in range(max_i, max_i - max_of_s, -1):
-                for j in range(max_j, max_j - max_of_s, -1):
-                    if State[i][j] != -1:
-                        done =True
 
         return State
